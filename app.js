258749f9c45ee970b0d5616cc7ddb0f8a390c791
Lin Htet Zaw <lynnhtetzaw.1137@gmail.com>
2024-09-07 21:10:57 +0630

    original

---
+++ app.js
@@ -0,0 +1,6 @@
+//I
+
+const getmodal = document.querySelector('.modal');
+const getmodalimg = document.querySelector('.modal-img');
+const getcaption = document.querySelector('.caption');
+const getbtnclose = document.querySelector('.btn-close');
